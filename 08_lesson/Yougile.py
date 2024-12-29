c2fd5dfc2f85f75582c5ccb556615ef2213182fe
Shamil29 <shamsudinov2911@gmail.com>
2024-12-29 16:45:47 +0300

    Lesson 8

--- 08_lesson/Yougile.py
+++ 08_lesson/Yougile.py
@@ -5,21 +5,6 @@ class Yougile:
     def __init__(self, url):
         self.url = url
 
-    # [POST]/api-v2/auth/keys - Создать ключ
-    # def create_key(self, my_authorization, my_content_type, my_login, my_password, company_id):
-    #     my_headers = {
-    #         'Authorization': my_authorization,
-    #         'Content-Type': my_content_type
-    #     }
-    #
-    #     auth = {
-    #         'login': my_login,
-    #         'password': my_password,
-    #         'companyId': company_id
-    #     }
-    #     resp = requests.post(self.url + '/auth/keys', headers=my_headers, json=auth)
-    #     return resp.json()
-
     # [POST]/api-v2/projects - Создать проект
     def create_project(self, my_authorization, my_content_type, my_title):
         my_headers = {
@@ -31,7 +16,8 @@ class Yougile:
             'title': my_title
         }
 
-        resp = requests.post(self.url + '/projects', headers=my_headers, json=body)
+        resp = requests.post(
+            self.url + '/projects', headers=my_headers, json=body)
         return resp
 
     # [GET]/api-v2/projects - Получить список проектов
@@ -52,18 +38,24 @@ class Yougile:
         my_project_id = {
             "id": project_id
         }
-        resp = requests.get(f"{self.url} + '/projects/{project_id}'", headers=my_headers)
+        resp = requests.get(
+            self.url + f"/projects/{project_id}", headers=my_headers)
         return resp
 
     # [PUT]/api-v2/projects/{id} - Изменить
-    def edit_by_id(self, my_authorization, my_content_type, new_id, new_title):
+    def edit_by_id(
+            self, my_authorization, my_content_type, project_id, new_title):
         my_headers = {
             "Authorization": my_authorization,
             "Content-Type": my_content_type
         }
-        param = {
-            "id": new_id,
+        id_project = {
+            "id": project_id,
+        }
+        body = {
             "title": new_title
         }
-        resp = requests.put(f"{self.url} + '/projects/{id}'", headers=my_headers, json=param)
-        return resp.json()
+
+        resp = requests.put(
+            self.url + f"/projects/{project_id}", headers=my_headers, json=body)
+        return resp
--- 08_lesson/test_yougile.py
+++ 08_lesson/test_yougile.py
@@ -1,26 +1,8 @@
-import requests
 from Yougile import Yougile
 
 base_url = Yougile("https://ru.yougile.com/api-v2")
 key = "g5ci5PegiSCIFP8LgP1bIjEKhyS8gP0QzIV0ex8RH0U+bkhFHJbQSsTcJVlPImNH"
-# def test_create_key():
-#     my_authorization = "Bearer NlzQ55ry-5iWlqWtHsedlREWu2vvdlIapBKmNO-nX+EnurlfIJ6YEoQNy85xFZYN"
-#     my_content_type = "application/json"
-#     my_login = "shamsudinov2911@gmail.com"
-#     my_password = "1!xFrostx!1"
-#     company_id = "d0b3ce73-ee47-4b29-b752-c976efd4f11c"
-#
-#     resp = base_url.create_key(my_authorization, my_content_type, my_login, my_password, company_id)
-#     assert len(resp) > 0
-#     assert 'key' in resp
-#
-# def test_create_key_negative():
-#     my_login = "shamsudinov2911@gmail.com"
-#     my_password = "1!xFrostx!1"
-#     company_id = "d0b3ce73-ee47-4b29-b752-c976efd4f11c"
-#
-#     resp = base_url.create_key(my_login, my_password, company_id)
-#     assert len(resp) > 0
+
 
 def test_create_project():
     my_authorization = "Bearer NlzQ55ry-5iWlqWtHsedlREWu2vvdlIapBKmNO-nX+EnurlfIJ6YEoQNy85xFZYN"
@@ -28,39 +10,30 @@ def test_create_project():
     my_title = "Football"
 
     # Получаем список проектов до создания нового
-    all_company_list = base_url.get_project_list(my_authorization, my_content_type)
-    len_before = len(all_company_list)
+    all_company_list = base_url.get_project_list(
+        my_authorization, my_content_type)
+    len_before = len(all_company_list.json()['content'])
 
     # Создаем новый проект
     resp = base_url.create_project(my_authorization, my_content_type, my_title)
-    assert resp.status_code
+    assert resp.status_code == 201
     assert 'id' in resp.json()
 
     # Получаем список проектов после создания нового проекта
     result = base_url.get_project_list(my_authorization, my_content_type)
-    len_after = len(result)
+    len_after = len(result.json()['content'])
     assert len_after - len_before == 1
-    assert len(resp) > 0
+
 
 def test_create_project_negative():
     my_authorization = "Bearer NlzQ55ry-5iWlqWtHsedlREWu2vvdlIapBKmNO-nX+EnurlfIJ6YEoQNy85xFZYN"
     my_content_type = "application/json"
     my_title = ""
 
-    # Получаем список проектов до создания нового
-    all_company_list = base_url.get_project_list(my_authorization, my_content_type)
-    len_before = len(all_company_list)
-
     # Создаем новый проект
     resp = base_url.create_project(my_authorization, my_content_type, my_title)
-    assert 'id' in resp.json()
+    assert resp.status_code == 400
 
-    # Получаем список проектов после создания нового проекта
-    result = base_url.get_project_list(my_authorization, my_content_type)
-    len_after = len(result)
-
-    assert len_after - len_before == 1
-    assert len(resp) > 0
 
 def test_get_project_list():
     my_authorization = "Bearer NlzQ55ry-5iWlqWtHsedlREWu2vvdlIapBKmNO-nX+EnurlfIJ6YEoQNy85xFZYN"
@@ -68,44 +41,49 @@ def test_get_project_list():
     my_title = "Football"
 
     # Создаем новый проект
-    create_project = base_url.create_project(my_authorization, my_content_type, my_title)
+    create_project = base_url.create_project(
+        my_authorization, my_content_type, my_title)
     assert 'id' in create_project.json()
 
     # Получаем список проектов после создания проекта
     resp = base_url.get_project_list(my_authorization, my_content_type)
+    assert resp.status_code == 200
     assert 'id' in resp.json()['content'][-1]
     assert 'title' in resp.json()['content'][-1]
     assert resp.json()['content'][-1]['title'] == my_title
 
+
 def test_get_project_list_negative():
+    my_authorization = ""
     my_content_type = "application/json"
     my_title = "Football"
 
     # Создаем новый проект
-    create_project = base_url.create_project(my_authorization, my_content_type, my_title)
-    assert 'id' in create_project
+    base_url.create_project(my_authorization, my_content_type, my_title)
 
     # Получаем список проектов после создания проекта
-    resp = base_url.get_project_list(my_content_type)
-    assert 'id' in resp[0]
-    assert 'title' in resp[2]
-    assert resp[-1]['title'] == my_title
+    resp = base_url.get_project_list(my_authorization, my_content_type)
+    assert resp.status_code == 404
+
 
 def test_get_id_project():
     my_authorization = "Bearer NlzQ55ry-5iWlqWtHsedlREWu2vvdlIapBKmNO-nX+EnurlfIJ6YEoQNy85xFZYN"
     my_content_type = "application/json"
-    my_title = "Football"
+    my_title = 'Football'
 
     # Создаем новый проект
-    create_project = base_url.create_project(my_authorization, my_content_type, my_title)
-    # assert 'id' in create_project
+    create_project = base_url.create_project(
+        my_authorization, my_content_type, my_title)
+    assert 'id' in create_project.json()
 
     project_id = create_project.json()['id']
 
     # Получаем по id проект после создания проекта
-    resp = base_url.get_id_project(my_authorization, my_content_type, project_id)
-    assert len(resp.json()) > 0
+    resp = base_url.get_id_project(
+        my_authorization, my_content_type, project_id)
+    assert resp.status_code == 200
     assert resp.json()['title'] == my_title
+
 
 def test_get_id_project_negative():
     my_authorization = "Bearer NlzQ55ry-5iWlqWtHsedlREWu2vvdlIapBKmNO-nX+EnurlfIJ6YEoQNy85xFZYN"
@@ -113,14 +91,16 @@ def test_get_id_project_negative():
     my_title = "Football"
 
     # Создаем новый проект
-    create_project = base_url.create_project(my_authorization, my_content_type, my_title)
-    assert 'id' in create_project
+    create_project = base_url.create_project(
+        my_authorization, my_content_type, my_title)
 
-    # Получаем проект после создания проекта
-    resp = base_url.get_id_project(my_authorization, my_content_type)
+    project_id = create_project.json()['id'] + f'1'
 
-    assert len(resp) > 0
-    assert resp['title'] == my_title
+    # Получаем по id проект после создания проекта
+    resp = base_url.get_id_project(
+        my_authorization, my_content_type, project_id)
+    assert resp.status_code == 404
+
 
 def test_edit_by_id():
     my_authorization = "Bearer NlzQ55ry-5iWlqWtHsedlREWu2vvdlIapBKmNO-nX+EnurlfIJ6YEoQNy85xFZYN"
@@ -129,22 +109,22 @@ def test_edit_by_id():
     new_title = "Fooball-Club"
 
     # Создаем новый проект
-    create_project = base_url.create_project(my_authorization, my_content_type, my_title)
-    assert 'id' in create_project
+    create_project = base_url.create_project(
+        my_authorization, my_content_type, my_title)
+    assert 'id' in create_project.json()
 
-    project_id = create_project['id']
+    project_id = create_project.json()['id']
 
     # Получаем проект после создания проекта
-    resp = base_url.get_id_project(my_authorization, my_content_type, project_id)
-    assert len(resp) > 0
+    resp = base_url.get_id_project(
+        my_authorization, my_content_type, project_id)
+    assert resp.json()['title'] == my_title
 
     # Изменяем проект по id
-    edit_project = base_url.edit_by_id(my_authorization, my_content_type, project_id, new_title)
-    assert 'id' in edit_project
-
-    # Получаем по id проект после редактирования проекта
-    resp = base_url.get_id_project(my_authorization, my_content_type, project_id)
-    assert resp['title'] == new_title
+    edit_project = base_url.edit_by_id(
+        my_authorization, my_content_type, project_id, new_title)
+    assert edit_project.status_code == 200
+    assert 'id' in edit_project.json()
 
 
 def test_edit_by_id_negative():
@@ -154,19 +134,13 @@ def test_edit_by_id_negative():
     new_title = "Fooball-Club"
 
     # Создаем новый проект
-    create_project = base_url.create_project(my_authorization, my_content_type, my_title)
-    assert 'id' in create_project
+    create_project = base_url.create_project(
+        my_authorization, my_content_type, my_title)
 
-    project_id = create_project['id']
-
-    # Получаем проект после создания проекта
-    resp = base_url.get_id_project(my_authorization, my_content_type, project_id)
-    assert len(resp) > 0
+    project_id = create_project.json()['id'] + f'1'
 
     # Изменяем проект по id
-    edit_project = base_url.edit_by_id(my_authorization, my_content_type, new_title)
-    assert 'id' in edit_project
+    edit_project = base_url.edit_by_id(
+        my_authorization, my_content_type, project_id, new_title)
 
-    # Получаем по id проект после редактирования проекта
-    resp = base_url.get_id_project(my_authorization, my_content_type, project_id)
-    assert resp['title'] == new_title
+    assert edit_project.status_code == 404
